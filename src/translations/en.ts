77992d1b3a660d459241088bc4325b36d1cfe437
Nicolaj Vinholt <nicvspz@gmail.com>
2021-02-28 16:49:01 +0100

    Add missing translation key

--- src/translations/en.ts
+++ src/translations/en.ts
@@ -4,6 +4,7 @@ export const englishDictionary: Dictionary = {
   callStarted: 'started a call in',
   cardImageCommandHelp: 'Searches for trading cards',
   helpCommandText: 'The following commands are available:',
+  topEmotesCommandHelp: 'Gets the top emotes for the channel',
   videoSearchCommandHelp: 'Searches for videos',
   noCardFound: 'No card result found',
   noEmotesFound: 'It seems like no emotes have been used in this channel yet!',
