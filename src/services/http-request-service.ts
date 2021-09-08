8d5df22906ba912f965b284835709da844d36f6f
Nicolaj Vinholt <nicvspz@gmail.com>
2021-09-08 15:51:52 +0200

    Corretly encode URIs

--- src/services/http-request-service.ts
+++ src/services/http-request-service.ts
@@ -3,7 +3,7 @@ import request = require('request');
 export class HttpRequestService implements RequestService {
   public getAsObject(requestUri: string): Promise<Record<string, unknown>> {
     return new Promise(resolve => {
-      request(requestUri, (error, _response, body) => {
+      request(encodeURI(requestUri), (error, _response, body) => {
         if (error) {
           throw new Error('Failed to fulfill request');
         }
